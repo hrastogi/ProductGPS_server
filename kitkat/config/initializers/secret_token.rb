1b6dd1b5f6b5a945155f8adf2d783d5b17df66b7
harshit rastogi <harshit@gogii.net>
2014-01-25 11:27:27 -0800

    first

---
+++ kitkat/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kitkat::Application.config.secret_token = 'adf5f0c834361104451c25185721c52b7720588a2b41159d58f137f4036c4332508aaa32273535dc5b6749c8781348a1ce53b8897ad040d2773ea69c0711c2e9'
